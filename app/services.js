26639091a86bbe809b64b6c749dfe591b63f871d
Jessica Walker <jessicacrotty@gmail.com>
2021-07-24 05:20:30 -0400

    Adds additional CRUD for members, criteria.

--- app/services.js
+++ app/services.js
@@ -33,6 +33,60 @@ var services = function(app) {
         });
     });
 
+    app.post("/members-add", function(req, res) {
+
+        var newMemberEntry = {
+            member_username: req.body.member_username,
+            member_role: req.body.member_role,
+            member_notes: req.body.member_note,
+            current_member: req.body.current_member
+        };
+
+        MongoClient.connect(dbURL, { useUnifiedTopology: true }, function(err, client) {
+            if (err) {
+                return res.status(200).send(JSON.stringify({ msg: "Error: " + err }));
+            } else {
+                var dbo = client.db("alliancemgr");
+
+                dbo.collection("members").insertOne(newMemberEntry, function(err, response) {
+                    if (err) {
+                        client.close();
+                        return res.status(200).send(JSON.stringify({ msg: "Error: " + err }));
+                    } else {
+                        client.close();
+                        return res.status(200).send(JSON.stringify({ msg: "SUCCESS" }));
+                    }
+                });
+            }
+        });
+    });
+
+    app.post("trackingcriteria-add", function(req, res) {
+
+        var newCriteriaEntry = {
+            criteria_name: req.body.criteria_name,
+            criteria_datatype: req.body.criteria_datatype
+        };
+
+        MongoClient.connect(dbURL, { useUnifiedTopology: true }, function(err, client) {
+            if (err) {
+                return res.status(200).send(JSON.stringify({ msg: "Error: " + err }));
+            } else {
+                var dbo = client.db("alliancemgr");
+
+                dbo.collection("trackingcriteria").insertOne(newCriteriaEntry, function(err, response) {
+                    if (err) {
+                        client.close();
+                        return res.status(200).send(JSON.stringify({ msg: "Error: " + err }));
+                    } else {
+                        client.close();
+                        return res.status(200).send(JSON.stringify({ msg: "SUCCESS" }));
+                    }
+                });
+            }
+        });
+    });
+
     // READ
     app.get("/read-records", function(req, res) {
         var sortBy = { date: -1, _id: 1 }; // go back to default sort
@@ -215,10 +269,10 @@ var services = function(app) {
     });
 
     // remove a record
-    app.delete("/delete-record", function(req, res) {
-        var userdataID = req.query.userdataID;
+    app.delete("/members-delete/:id", function(req, res) {
+        var memberID = req.params.id;
 
-        var s_id = new ObjectId(userdataID);
+        var s_id = new ObjectId(memberID);
         var search = { _id: s_id };
 
         MongoClient.connect(dbURL, { useUnifiedTopology: true }, function(err, client) {
@@ -227,7 +281,31 @@ var services = function(app) {
             } else {
                 var dbo = client.db("alliancemgr");
 
-                dbo.collection("userdata").deleteOne(search, function(err, response) {
+                dbo.collection("members").deleteOne(search, function(err, response) {
+                    if (err) {
+                        return res.status(200).send(JSON.stringify({ msg: "Error: " + err }));
+                    } else {
+                        client.close();
+                        return res.status(200).send(JSON.stringify({ msg: "SUCCESS" }));
+                    }
+                });
+            }
+        });
+    });
+    
+    app.delete("/trackingcriteria-delete/:id", function(req, res) {
+        var trackingcriteriaID = req.params.id;
+
+        var s_id = new ObjectId(trackingcriteriaID);
+        var search = { _id: s_id };
+
+        MongoClient.connect(dbURL, { useUnifiedTopology: true }, function(err, client) {
+            if (err) {
+                return res.status(200).send(JSON.stringify({ msg: "Error: " + err }));
+            } else {
+                var dbo = client.db("alliancemgr");
+
+                dbo.collection("trackingcriteria").deleteOne(search, function(err, response) {
                     if (err) {
                         return res.status(200).send(JSON.stringify({ msg: "Error: " + err }));
                     } else {
@@ -276,6 +354,84 @@ var services = function(app) {
             }
         });
     });
+
+
+    app.put("/members-update/:id", function(req, res) {
+        console.log("Begin update");
+        var memberID = req.params.id;
+        var member_username = req.body.member_username;
+        var member_role = req.body.member_role;
+        var member_notes = req.body.member_notes;
+        var current_member = req.body.current_member;
+
+        var s_id = new ObjectId(memberID);
+
+        var search = { _id: s_id };
+
+        var updateData = {
+            $set: {
+                member_username: member_username,
+                member_role: member_role,
+                member_notes: member_notes,
+                current_member: current_member
+            }
+        };
+
+        MongoClient.connect(dbURL, { useUnifiedTopology: true }, function(err, client) {
+            if (err) {
+                return res.status(200).send(JSON.stringify({ msg: "Error: " + err }));
+            } else {
+                var dbo = client.db("alliancemgr");
+
+                dbo.collection("members").updateOne(search, updateData, function(err, response) {
+                    if (err) {
+                        client.close();
+                        return res.status(200).send(JSON.stringify({ msg: "Error: " + err }));
+                    } else {
+                        client.close();
+                        return res.status(200).send(JSON.stringify({ msg: "SUCCESS" }));
+                    }
+                });
+            }
+        });
+    });
+
+    app.put("/trackingcriteria-update/:id", function(req, res) {
+        console.log("Begin update");
+        var criteriaID = req.params.id;
+        var criteria_name = req.body.criteria_name;
+        var criteria_datatype = req.body.criteria_datatype;
+
+        var s_id = new ObjectId(criteriaID);
+
+        var search = { _id: s_id };
+
+        var updateData = {
+            $set: {
+                criteria_name: criteria_name,
+                criteria_datatype: criteria_datatype
+            }
+        };
+
+        MongoClient.connect(dbURL, { useUnifiedTopology: true }, function(err, client) {
+            if (err) {
+                return res.status(200).send(JSON.stringify({ msg: "Error: " + err }));
+            } else {
+                var dbo = client.db("alliancemgr");
+
+                dbo.collection("trackingcriteria").updateOne(search, updateData, function(err, response) {
+                    if (err) {
+                        client.close();
+                        return res.status(200).send(JSON.stringify({ msg: "Error: " + err }));
+                    } else {
+                        client.close();
+                        return res.status(200).send(JSON.stringify({ msg: "SUCCESS" }));
+                    }
+                });
+            }
+        });
+    });
+
 
     app.put("/update-record", function(req, res) {
         var userdataID = req.body.userdataID;
